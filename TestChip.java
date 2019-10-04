1375aeb33087f7e3b799a89b634d8eef17710dfa
Marcus Peixoto <46487114+MarcusPeixe@users.noreply.github.com>
2019-10-04 09:38:33 -0300

    Testing programs and memory dump

--- TestChip.java
+++ TestChip.java
@@ -7,9 +7,13 @@ class TestChip {
 
 		MyMapper() {
 			ram = new int[RAM_SIZE];
+			// final int[] program = {
+			// 	0xE6, 0x02, 0xA5, 0x02, 0xC9, 0x0A, 0x30, 0xF8,
+			// 	0x00, 0xEA, 0x00, 0xEA, 0x4C, 0x0C, 0x02
+			// };
 			final int[] program = {
-				0xE6, 0x02, 0xA5, 0x02, 0xC9, 0x0A, 0x30, 0xF8,
-				0x00, 0xEA, 0x00, 0xEA, 0x4C, 0x0C, 0x02
+				0XA2, 0X08, 0X8A, 0X95, 0X08, 0XE8, 0XD0, 0XFA,
+				0XF0, 0XFE
 			};
 			for (int i = 0; i < RAM_SIZE; i++) {
 				ram[i] = 0x00;
@@ -36,13 +40,45 @@ class TestChip {
 		public void onVectorPull(int addr) {
 			System.out.printf("Pulling vector $%04X\n", addr);
 		}
+
+		public void memDump(int start, int end, int columns) {
+			if (columns < 1) return;
+			int startCbytes = start / columns * columns;
+			int endCbytes = (end + columns - 1) / columns * columns;
+			// System.out.printf("$%04X = ", startCbytes);
+			for (int i = startCbytes; i < start; i++) {
+				if (i % 256 == 0)
+					System.out.printf("\nPAGE CROSS: ");
+				if (i % columns == 0)
+					System.out.printf("\n$%04X = ", i);
+				System.out.printf("-- ");
+			}
+			for (int i = start; i <= end; i++) {
+				if (i % 256 == 0)
+					System.out.printf("\nPAGE CROSS: ");
+				if (i % columns == 0)
+					System.out.printf("\n$%04X = ", i);
+				System.out.printf("%02X ", ram[i]);
+			}
+			for (int i = end + 1; i < endCbytes; i++) {
+				if (i % 256 == 0)
+					System.out.printf("\nPAGE CROSS: ");
+				if (i % columns == 0)
+					System.out.printf("\n$%04X = ", i);
+				System.out.printf("-- ");
+			}
+			System.out.println();
+		}
 	}
 
 	public static void main(String[] args) {
-		Chip65C02 chip = new Chip65C02(new MyMapper());
+		MyMapper mapper = new MyMapper();
+		Chip65C02 chip = new Chip65C02(mapper);
 		chip.setReady(true);
-		for (int i = 0; i < 200; i++) {
+		int loopCount = 260;
+		for (int i = 0; i < (loopCount * 10 + 2); i++) {
 			chip.tickClock();
 		}
+		mapper.memDump(0x0000, 0x02ff, 16);
 	}
 }
